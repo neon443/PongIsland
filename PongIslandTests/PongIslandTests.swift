6c4c9072428d5fb667f632e376933cd6b8921fc9
neon443 <69979447+neon443@users.noreply.github.com>
2024-12-04 19:47:21 +0000

    Initial Commit

---
+++ PongIslandTests/PongIslandTests.swift
@@ -0,0 +1,17 @@
+//
+//  PongIslandTests.swift
+//  PongIslandTests
+//
+//  Created by Nihaal Sharma on 04/12/2024.
+//
+
+import Testing
+@testable import PongIsland
+
+struct PongIslandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
